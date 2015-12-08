a198b3a5c5b41aeddadf556cfec88b37a051f086
b79789 <bstacks78@fullsail.edu>
2015-12-08 13:48:27 -0500

    Building Folders and Project Start
    Building folders for the start of our final project for Full Sail
    University

---
+++ Sportify/Sportify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sportify
+//
+//  Created by Brian Stacks on 12/2/15.
+//  Copyright © 2015 Brian Stacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sportify/Sportify/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sportify
+//
+//  Created by Brian Stacks on 12/2/15.
+//  Copyright © 2015 Brian Stacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
